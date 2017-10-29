0fbdc406480903d89d0f23c55e0d8461016bc886
jobtalle <jobtalle@hotmail.com>
2017-10-29 14:55:57 +0100

    Fixed typo

--- js/loadmore.js
+++ js/loadmore.js
@@ -4,10 +4,8 @@ function loadMore() {
 	var request = new XMLHttpRequest();
 	request.open("GET", "index" + currentIndex++ + ".html", true);
 	request.onload = function() {
-		if(request.status >= 200 && request.status < 400) {
-			document.getElementById("content").innerHtml += request.responseText;
-			alert(request.responseText);
-		}
+		if(request.status >= 200 && request.status < 400)
+			document.getElementById("content").innerHTML += request.responseText;
 	}
 	
 	request.send();
