78b698ae780716899585c48ffd2a3fd1682ec27c
jobtalle <jobtalle@hotmail.com>
2017-10-30 23:43:27 +0100

    Added new index

--- js/loadmore.js
+++ js/loadmore.js
@@ -2,7 +2,7 @@ currentIndex = 1;
 
 function loadMore() {
 	var nextRequest = new XMLHttpRequest();
-		
+	
 	nextRequest.open("HEAD", "index" + (currentIndex + 1) + ".html", true);
 	nextRequest.onloadend = function() {
 		if(nextRequest.status == 404)
