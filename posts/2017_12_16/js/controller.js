8f9d77775b5350c1531619f23075d5bfe7ab799c
jobtalle <jobtalle@hotmail.com>
2017-12-24 15:35:35 +0100

    Zooming on lindenmayer system is only possible on large devices

--- posts/2017_12_16/js/controller.js
+++ posts/2017_12_16/js/controller.js
@@ -33,7 +33,21 @@ Controller.prototype = {
 		element.addEventListener("mouseup", this.mouseUp.bind(this));
 		element.addEventListener("touchend", this.mouseUp.bind(this));
 		element.addEventListener("mouseleave", this.mouseUp.bind(this));
+		element.addEventListener("mousewheel", this.mouseScroll.bind(this));
 	},
+	
+	mouseScroll(event) {
+		console.log(document.documentElement.clientWidth);
+		if(document.documentElement.clientWidth <= 480)
+			return;
+		
+		event.preventDefault();		
+	
+		if(event.deltaY < 0)		
+			this.renderer.zoomIn();		
+		else		
+			this.renderer.zoomOut();		
+  	},
 	
 	touchDown(event) {
 		this.dragStart(event.touches[0].clientX - this.rendererRect.left, event.touches[0].clientY - this.rendererRect.top);
