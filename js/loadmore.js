62e1e0a40e0f8d731948667c6aa68e267fe16336
Job <jobtalle@hotmail.com>
2019-08-01 14:39:40 +0200

    Fixed load more feature

--- js/loadmore.js
+++ js/loadmore.js
@@ -5,17 +5,20 @@ function loadMore() {
 	
 	nextRequest.open("HEAD", "index" + (currentIndex + 1) + ".html", true);
 	nextRequest.onloadend = function() {
-		if(nextRequest.status == 404)
-			document.getElementById("content-footer").innerHTML = "";
+		if(nextRequest.status === 404)
+			document.getElementById("load-more").innerHTML = "";
 		
 		var request = new XMLHttpRequest();
 		
 		request.open("GET", "index" + currentIndex++ + ".html", true);
 		request.onload = function() {
+			console.log(request.status);
 			if(request.status >= 200 && request.status < 400)
 				document.getElementById("content").innerHTML += request.responseText;
-		}
+		};
+
 		request.send();
-	}
+	};
+
 	nextRequest.send();
 }
