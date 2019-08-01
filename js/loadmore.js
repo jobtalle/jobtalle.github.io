cac39ef4a36d904562e4cc40c73da0f3912531c1
Job <jobtalle@hotmail.com>
2019-08-01 14:45:55 +0200

    loadmore.js no longer scrolls

--- js/loadmore.js
+++ js/loadmore.js
@@ -5,15 +5,16 @@ function loadMore() {
 	
 	nextRequest.open("HEAD", "index" + (currentIndex + 1) + ".html", true);
 	nextRequest.onloadend = function() {
-		if(nextRequest.status === 404)
-			document.getElementById("load-more").innerHTML = "";
-		
 		var request = new XMLHttpRequest();
 		
 		request.open("GET", "index" + currentIndex++ + ".html", true);
 		request.onload = function() {
-			if(request.status >= 200 && request.status < 400)
+			if(request.status >= 200 && request.status < 400) {
 				document.getElementById("content").innerHTML += request.responseText;
+
+				if (nextRequest.status === 404)
+					document.getElementById("load-more").innerHTML = "";
+			}
 		};
 
 		request.send();
