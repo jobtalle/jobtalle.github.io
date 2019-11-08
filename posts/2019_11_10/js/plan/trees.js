9fb906e0d5f769aab66911b9466791ec47709837
jobtalle <jobtalle@hotmail.com>
2019-11-08 16:31:59 +0100

    Trees are a bit denser

--- posts/2019_11_10/js/plan/trees.js
+++ posts/2019_11_10/js/plan/trees.js
@@ -39,7 +39,7 @@ const Trees = function(height, heightmap, bounds, lighting, scale) {
     };
 };
 
-Trees.VOLUME_DENSITY = 0.5;
+Trees.VOLUME_DENSITY = 0.7;
 Trees.RADIUS_MIN = 6;
 Trees.RADIUS_MAX = 12;
 Trees.HEIGHT_FACTOR_MIN = 1.8;
