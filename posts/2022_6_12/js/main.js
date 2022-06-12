8bf5f680f5c476a5f98db7158103915a8b0ba6f2
Job <jobtalle@hotmail.com>
2022-06-12 21:48:28 +0200

    Simplified GLSL randomizer

--- posts/2022_6_12/js/main.js
+++ posts/2022_6_12/js/main.js
@@ -83,6 +83,7 @@ import {Mesh} from "./mesh.js";
     gl.bindTexture(gl.TEXTURE_2D, texturePattern);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, width << 1, height << 1, 0, gl.RGB, gl.UNSIGNED_BYTE, null);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
@@ -92,6 +93,7 @@ import {Mesh} from "./mesh.js";
     gl.bindTexture(gl.TEXTURE_2D, textureShape);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width << 1, height << 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
--- posts/2022_6_12/js/shaderSpots.js
+++ posts/2022_6_12/js/shaderSpots.js
@@ -3,63 +3,46 @@ import {gl} from "./gl.js";
 
 export class ShaderSpots extends Shader {
     static FRAGMENT = `#version 300 es
-        mediump vec4 mod289(mediump vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
-        mediump vec4 perm(mediump vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
-
-        mediump float random3(mediump vec3 p) {
-            mediump vec3 a = floor(p);
-            mediump vec3 d = p - a;
-            d = d * d * (3.0 - 2.0 * d);
-        
-            mediump vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
-            mediump vec4 k1 = perm(b.xyxy);
-            mediump vec4 k2 = perm(k1.xyxy + b.zzww);
-        
-            mediump vec4 c = k2 + a.zzzz;
-            mediump vec4 k3 = perm(c);
-            mediump vec4 k4 = perm(c + 1.0);
-        
-            mediump vec4 o1 = fract(k3 * (1.0 / 41.0));
-            mediump vec4 o2 = fract(k4 * (1.0 / 41.0));
-        
-            mediump vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
-            mediump vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
-        
-            return o4.y * d.y + o4.x * (1.0 - d.y);
+        mediump float random3(highp vec3 p) {
+            return fract(sin(dot(p, vec3(12.9898, 78.2331, 42.1914))) * 43758.5452);
         }
         
         mediump float interpolate(mediump float a, mediump float b, mediump float c, mediump float d, mediump float x) {
             mediump float p = (d - c) - (a - b);
+            
             return x * (x * (x * p + ((a - b) - p)) + (c - a)) + b;
         }
     
         mediump float sampleX(mediump vec3 at) {
             mediump float floored = floor(at.x);
+            
             return interpolate(
-                random3(vec3(floored - 1.0, at.yz)),
+                random3(vec3(floored - 1., at.yz)),
                 random3(vec3(floored, at.yz)),
-                random3(vec3(floored + 1.0, at.yz)),
-                random3(vec3(floored + 2.0, at.yz)),
-                at.x - floored) * 0.5 + 0.25;
+                random3(vec3(floored + 1., at.yz)),
+                random3(vec3(floored + 2., at.yz)),
+                at.x - floored) * .5 + .25;
         }
         
         mediump float sampleY(mediump vec3 at) {
             mediump float floored = floor(at.y);
+            
             return interpolate(
-                sampleX(vec3(at.x, floored - 1.0, at.z)),
+                sampleX(vec3(at.x, floored - 1., at.z)),
                 sampleX(vec3(at.x, floored, at.z)),
-                sampleX(vec3(at.x, floored + 1.0, at.z)),
-                sampleX(vec3(at.x, floored + 2.0, at.z)),
+                sampleX(vec3(at.x, floored + 1., at.z)),
+                sampleX(vec3(at.x, floored + 2., at.z)),
                 at.y - floored);
         }
         
         mediump float cubicNoise(mediump vec3 at) {
             mediump float floored = floor(at.z);
+            
             return interpolate(
-                sampleY(vec3(at.xy, floored - 1.0)),
+                sampleY(vec3(at.xy, floored - 1.)),
                 sampleY(vec3(at.xy, floored)),
-                sampleY(vec3(at.xy, floored + 1.0)),
-                sampleY(vec3(at.xy, floored + 2.0)),
+                sampleY(vec3(at.xy, floored + 1.)),
+                sampleY(vec3(at.xy, floored + 2.)),
                 at.z - floored);
         }
 
